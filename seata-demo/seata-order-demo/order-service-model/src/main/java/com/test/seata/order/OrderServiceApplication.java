af43192761100cfcd14050e0e599a56e1365e62d
liujian <1090182260@qq.com>
2020-04-02 18:11:48 +0800

    功能提交

--- seata-demo/seata-order-demo/order-service-model/src/main/java/com/test/seata/order/OrderServiceApplication.java
+++ seata-demo/seata-order-demo/order-service-model/src/main/java/com/test/seata/order/OrderServiceApplication.java
@@ -1,13 +1,12 @@
 package com.test.seata.order;
 
-import com.alibaba.cloud.seata.GlobalTransactionAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = GlobalTransactionAutoConfiguration.class)
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.test.seata.order.mapper")
