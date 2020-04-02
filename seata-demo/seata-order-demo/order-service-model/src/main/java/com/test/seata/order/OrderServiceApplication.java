046073a047f72c94ca44e409b5792f211d37f5e1
liujian <1090182260@qq.com>
2020-04-02 17:58:40 +0800

    功能提交

--- seata-demo/seata-order-demo/order-service-model/src/main/java/com/test/seata/order/OrderServiceApplication.java
+++ seata-demo/seata-order-demo/order-service-model/src/main/java/com/test/seata/order/OrderServiceApplication.java
@@ -1,13 +1,13 @@
 package com.test.seata.order;
 
+import com.alibaba.cloud.seata.GlobalTransactionAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = GlobalTransactionAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.test.seata.order.mapper")
--- seata-demo/seata-user-demo/user-sevice-model/src/main/java/com/test/seata/user/UserServiceApplication.java
+++ seata-demo/seata-user-demo/user-sevice-model/src/main/java/com/test/seata/user/UserServiceApplication.java
@@ -1,12 +1,13 @@
 package com.test.seata.user;
 
+import com.alibaba.cloud.seata.GlobalTransactionAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = GlobalTransactionAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.test.seata.user.mapper")
