f04d8195e729505b3cb8c11f87788a86b939f24d
liujian <1090182260@qq.com>
2020-04-14 18:04:07 +0800

    rocketmq 功能提交

---
+++ rocketmq-demo/rocketmq-consume-demo/src/main/java/com/test/rocketmq/consume/MQConsumeApplication.java
@@ -0,0 +1,18 @@
+package com.test.rocketmq.consume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liujian
+ * @ProjectName springcloud-alibaba
+ * @Description: MQConsumeApplication
+ * @date 2020/4/14  13:37
+ */
+@SpringBootApplication
+public class MQConsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MQConsumeApplication.class, args);
+    }
+}
---
+++ rocketmq-demo/rocketmq-producer-demo/src/main/java/com/test/rocketmq/produce/MQProduceApplication.java
@@ -0,0 +1,18 @@
+package com.test.rocketmq.produce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liujian
+ * @ProjectName springcloud-alibaba
+ * @Description: MQProduceApplication
+ * @date 2020/4/14  13:38
+ */
+@SpringBootApplication
+public class MQProduceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MQProduceApplication.class,args);
+    }
+}
